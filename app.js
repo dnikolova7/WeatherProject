cfefd560428a4e49a8dcfc225717d4db6b0fbfa5
Daniela Nikolova <dnikolova.inbox@gmail.com>
2023-03-13 16:57:18 +0200

    I've done some changes.

--- app.js
+++ app.js
@@ -1,35 +1,55 @@
 const express = require ("express");
 const https = require ("https");
-import ('node-fetch');
+// const { send } = require("process");
+// import ('node-fetch');
+const bodyParser = require ("body-parser");
 
 const app = express ();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=42.698334&lon=23.319941&appid=b813ad9ec2d7b9fb72fcfd6c0804416a&units=metric";
-    // let settings = { method: "Get" };
+   res.sendFile(__dirname + "/index.html");
 
-    // fetch(url, settings)
-    // .then(res => res.json())
-    // .then((json) => {
-    //     console.log(json);
-        // do something with JSON
-    // });
+    // res.send("Server is up and running");
+})
 
-    https.get(url, function(response){
-        console.log(response.statusCode);
-
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            console.log(temp);
-            const desc = weatherData.weather[0].description;
-            console.log (desc);
-            
-        });
+app.post ("/", function(req, res){
+console.log("Post request recieved");
 });
 
-    res.send("Server is up and running");
-})
+// const query = "Sofia";
+// const apiKey = "b813ad9ec2d7b9fb72fcfd6c0804416a";
+// const units = "Celsius";
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
+// // let settings = { method: "Get" };
+
+// // fetch(url, settings)
+// // .then(res => res.json())
+// // .then((json) => {
+// //     console.log(json);
+//     // do something with JSON
+// // });
+
+// https.get(url, function(response){
+//     console.log(response.statusCode);
+
+//     response.on("data", function(data){
+//         const weatherData = JSON.parse(data);
+//         const temp = weatherData.main.temp;
+       
+//         const desc = weatherData.weather[0].description;
+
+//         const icon = weatherData.weather[0].icon
+
+//         const iconImage = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+//         res.write ("<h1>The temperature in Sofia is " + temp + " degrees Celsius."+ "<p>The weather is currently " + desc + ". </h1>" );
+//         res.write ("<img src = " + iconImage + ">");
+//         res.send();
+        
+//     });
+// });
 
 app.listen(3000, function(){
     console.log ("I am port 3000");

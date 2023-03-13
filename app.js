4d74edec769b6e08be6707b13f51476dec49fd19
Daniela Nikolova <dnikolova.inbox@gmail.com>
2023-03-13 17:31:56 +0200

    Final Weather Project

--- app.js
+++ app.js
@@ -15,41 +15,42 @@ app.get("/", function(req, res){
 })
 
 app.post ("/", function(req, res){
-console.log("Post request recieved");
+   
+const query = req.body.cityName;
+const apiKey = "b813ad9ec2d7b9fb72fcfd6c0804416a";
+// const units = "Celsius";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=metric";
+// let settings = { method: "Get" };
+
+// fetch(url, settings)
+// .then(res => res.json())
+// .then((json) => {
+//     console.log(json);
+    // do something with JSON
+// });
+
+https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+       
+        const desc = weatherData.weather[0].description;
+
+        const icon = weatherData.weather[0].icon
+
+        const iconImage = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+        res.write ("<h1>The temperature in " + query + " is " + temp + " degrees Celsius."+ "<p>The weather is currently " + desc + ". </h1>" );
+        res.write ("<img src = " + iconImage + ">");
+        res.send();
+        
+    });
 });
 
-// const query = "Sofia";
-// const apiKey = "b813ad9ec2d7b9fb72fcfd6c0804416a";
-// const units = "Celsius";
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units;
-// // let settings = { method: "Get" };
+});
 
-// // fetch(url, settings)
-// // .then(res => res.json())
-// // .then((json) => {
-// //     console.log(json);
-//     // do something with JSON
-// // });
-
-// https.get(url, function(response){
-//     console.log(response.statusCode);
-
-//     response.on("data", function(data){
-//         const weatherData = JSON.parse(data);
-//         const temp = weatherData.main.temp;
-       
-//         const desc = weatherData.weather[0].description;
-
-//         const icon = weatherData.weather[0].icon
-
-//         const iconImage = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-
-//         res.write ("<h1>The temperature in Sofia is " + temp + " degrees Celsius."+ "<p>The weather is currently " + desc + ". </h1>" );
-//         res.write ("<img src = " + iconImage + ">");
-//         res.send();
-        
-//     });
-// });
 
 app.listen(3000, function(){
     console.log ("I am port 3000");
